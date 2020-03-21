93035a68fae8f389a5cad5f16ccc5b82176059ea
Yunus Kerem Turk <krmtrk20@gmail.com>
2020-03-21 23:06:32 +0300

    Campground post route has set with its view

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ encoded: true }));
+
 var campgrounds = [
     {
         name: "Salmon Creek",
@@ -36,6 +39,23 @@ app.get("/", (req, res) => {
 
 app.get("/campgrounds", (req, res) => {
     res.render("campgrounds", { campgrounds: campgrounds });
+});
+
+//one is get one is post
+app.post("/campgrounds", (req, res) => {
+    //retrieve data from form
+    var name = req.body.name;
+    var image = req.body.image;
+
+    //push to the array
+    var newCampground = { name: name, image: image };
+    campgrounds.push(newCampground);
+    //redirect to the get campgrounds page.
+    res.redirect("/campgrounds");
+});
+
+app.get("/campgrounds/new", (req, res) => {
+    res.render("new");
 });
 
 app.listen(3000, () => {
